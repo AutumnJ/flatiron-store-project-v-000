034a6c27d086722d22fe54f7c4bb941712739165
Autumn Kangas <autumnkangas@gmail.com>
2018-03-06 15:26:34 -0600

    Done.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,13 @@
 class OrdersController < ApplicationController
 
   def show
-    @order = Order.find_by(id: params[:id], user_id: current_user.id)
-    if !@order
+    if !user_signed_in?
       redirect_to store_path
+    else 
+      @order = Order.find_by(id: params[:id], user_id: current_user.id)
+      if !@order
+        redirect_to store_path
+      end
     end
   end
 
